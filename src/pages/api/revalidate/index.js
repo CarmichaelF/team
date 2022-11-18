a8e28b52717f0c69e9bac8ca492155dfd3d9281a
CarmichaelF <carmichaelferreira06@gmail.com>
2022-11-18 19:39:33 -0300

    feat: adding revalidate

--- src/pages/api/revalidate/index.js
+++ src/pages/api/revalidate/index.js
@@ -1,17 +1,17 @@
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.NEXT_MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json({ message: "Invalid token" });
   }
 
   try {
-    console.log('Revalidating...')
-    await res.unstable_revalidate("/");
-    await res.unstable_revalidate("/blog");
+    await res.revalidate("/");
+    await res.revalidate("/blog");
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    console.log("error", err);
     return res.status(500).send("Error revalidating");
   }
 }
